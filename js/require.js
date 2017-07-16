f71c603b5f998771bd5b73582a7dc84ced511065
senschen <sens1992@qq.com>
2017-07-16 21:10:54 +0800

    fix重复加载 优化代码

--- js/require.js
+++ js/require.js
@@ -1,73 +1,72 @@
-let global = {
-    currScript: null,
-    finishModule: {}
-};
-function require(dept,cb) {
-    let deptLen = dept.length;
-    let arg = [];
-    dept.forEach(function (src,index) {
-        if(global.finishModule[src]){
-            updateState(src);
-        }
-        else {
-            let node = creatNode(src,index);
-            node.onload = function () {
-                global.currScript = node;
-            };
-            node.addEventListener('finish',function () {
-                updateState(src,index);
+(function () {
+    let global = {
+        currScript: null,
+        finishModule: {},
+        loadingModule: {}
+    };
+
+    function require(dept, cb) {
+        let node = document.currentScript || global.currScript; //currentScript兼容。实际上写法有误：由于这里先于node.onload事件处理函数执行，所以global.currScript应该改在nextTick去取
+        let deptLen = dept.length;
+        let arg = [];
+        if (deptLen) {
+            dept.forEach(function (src, index) {
+                if (global.finishModule[src]) {
+                    updateState(src, index);
+                }
+                else {
+                    if (global.loadingModule[src]) {
+                        let node = global.loadingModule[src];
+                        node.addEventListener('finish', function () {
+                            updateState(src, index);
+                        });
+                    }
+                    else {
+                        let node = creatNode(src);
+                        global.loadingModule[src] = node;
+                        node.onload = function () {
+                            global.currScript = node;
+                        };
+                        node.addEventListener('finish', function () {
+                            updateState(src, index);
+                            node.remove();
+                        });
+                    }
+                }
             });
         }
-    });
-    function updateState(src,index){
-        deptLen --;
-        arg[index] = global.finishModule[src];
-        if(deptLen === 0){
-            cb.apply(this,arg);
-        }
-    }
-}
-function define(dept,cb) {
-    let node = document.currentScript || global.currScript; //currentScript兼容。实际上写法有误：由于这里先于node.onload事件处理函数执行，所以global.currScript应该改在nextTick去取
-    let deptLen = dept.length;
-    let arg = [];
-    if (deptLen) {
-        dept.forEach(function (src, index) {
-            if (global.finishModule[src]) {
-                updateState(src,index);
-            }
-            else {
-                let node = creatNode(src);
-                node.onload = function () {
-                    global.currScript = node;
-                };
-                node.addEventListener('finish', function () {
-                    updateState(src,index);
-                });
-            }
-        });
-    }
-    else {
-        finishDef(arg);
-    }
-    function updateState(src,index) {
-        deptLen--;
-        arg[index] = global.finishModule[src];
-        if (deptLen === 0) {
+        else {
             finishDef();
         }
+
+        function updateState(src, index) {
+            deptLen--;
+            arg[index] = global.finishModule[src];
+            if (deptLen === 0) {
+                finishDef();
+            }
+        }
+
+        function finishDef() {
+            if (node && node.getAttribute('src')) {
+                global.finishModule[node.getAttribute('src')] = cb.apply(this, arg);
+                let evt = document.createEvent("HTMLEvents");
+                evt.initEvent("finish", false, false);
+                node.dispatchEvent(evt);
+            }
+            else {
+                cb.apply(this, arg);
+            }
+        }
     }
-    function finishDef() {
-        global.finishModule[node.getAttribute('src')] = cb.apply(this, arg);
-        let evt = document.createEvent("HTMLEvents");
-        evt.initEvent("finish", false, false);
-        node.dispatchEvent(evt);
+
+    function creatNode(src) {
+        let node = document.createElement('script');
+        node.async = true;
+        document.body.appendChild(node);
+        node.src = src;
+        return node;
     }
-}
-function creatNode(src) {
-    let node = document.createElement('script');
-    node.async = true;
-    document.body.appendChild(node);
-    node.src = src;
-    return node;
-}
+
+    window.define = window.require = require;
+})();
